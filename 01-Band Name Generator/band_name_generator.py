eb77f69d59d5d19b419e493a2eb8f97c880175ee
Ana Clara <aclara.bvicente@gmail.com>
2022-12-06 18:27:15 -0300

    Primeiro projeto: band name generator

---
+++ 01-Band Name Generator/band_name_generator.py
@@ -0,0 +1,8 @@
+# 100 Days of Python - Day 01
+
+greeting = print("Welcome to the band name generator!")
+
+city_name = input("Please, type the name of the city you grew up in: ")
+pet_name = input("Please, type the name of a pet: ")
+
+print(f"Your band name is {city_name} {pet_name}")
