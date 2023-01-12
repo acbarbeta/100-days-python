e15ae0c1bfb66324fbcc1062cfbed7827abe4a48
Ana Clara <aclara.bvicente@gmail.com>
2023-01-11 22:40:57 -0300

    caesar cipher

--- 07 - Caesar Cipher/caesar_cipher.py
+++ 07 - Caesar Cipher/caesar_cipher.py
@@ -1,19 +1,26 @@
 alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt: ")
-initial_text = input("Type your message: ").lower()
+initial_text = input("Type your message: ").upper()
 shift_number = int(input("Type the shift number: "))
 
-def caesar_decryption(start_text, shift):
+def caesar(start_text, shift, cipher_direction):
     end_text = ""
+
+    if cipher_direction == 'encode':
+        shift *= -1
 
     for char in start_text:
 
         if char in alphabet:
 
             position = alphabet.index(char)
-            new_position = position - shift_number
+            new_position = position + shift
             end_text += alphabet[new_position]
+        else:
+            end_text += char
+    
+    print(f"The result is {end_text}")
 
-    print(f"The decoded text is {end_text}")
+caesar(start_text=initial_text, shift=shift_number, cipher_direction=direction)
 
